73a478a396a13d06f2d2080480a8463660efb5fb
Juan Camilo Bastidas <juank2411@outlook.com>
2021-07-12 12:10:41 -0500

    borrar comentarios

--- src/main/java/co/edu/utp/misiontic2022/c2/jbastidas/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/jbastidas/App.java
@@ -13,24 +13,10 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) throws IOException {
-       // var input = new BufferedReader(new InputStreamReader(System.in));
-        
-        //System.out.print("Por Favor ingrese un numero:");
-        //var entrada = input.readLine ();
-        //var numero = Integer.parseInt(entrada);
-        //System.out.printf("El numero tiene %d cifras\n", numDigitos(numero));
-
-        //input.close();
 
         System.out.println(numeroSuerte("12/02/1993"));
     }
 
-    public static String saludo(String nombre){
-        return "hola "+ nombre + "!";
-    }
-
-
-    public static int numDigitos(int numero){
 
         
         var contador = 0;
